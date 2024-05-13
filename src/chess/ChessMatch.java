9eb4bf2521e11305e1cff82178579ef6920231cb
Hugo França <hugo.francaa06@gmail.com>
2024-05-13 11:03:34 -0300

    Rainha

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -188,6 +188,7 @@ public class ChessMatch {
 
     private void initialSetup() {
 
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
@@ -204,6 +205,7 @@ public class ChessMatch {
         placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
